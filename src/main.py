9d8cdcf58512602b8ee8105d6dc98765f3a85640
mehrdadzakershahrak <zaker.mehrdad@gmail.com>
2023-12-18 09:54:05 -0600

    task pipeline execution

--- src/main.py
+++ src/main.py
@@ -16,8 +16,15 @@ def main():
     research_task = create_task("Research Task", ['research'], {'research': research_instructions})
     drafting_task = create_task("Drafting Task", ['drafting'], {'drafting': drafting_instructions})
 
-    # Create a pipeline
-    pipeline = TaskPipeline([research_task, drafting_task])
+    # Create a pipeline of tasks
+    resource_manager = ResourceManager(max_concurrent_tasks=3)
+    pipeline = TaskPipeline([research_task, drafting_task], resource_manager)
+
+    # Execute tasks in parallel with resource management
+    parallel_results = pipeline.execute_in_parallel(framework, [('input1',), ('input2',)])
+
+    print(f"Parallel Execution Results with Resource Management: {parallel_results}")
+
 
     # Execute tasks sequentially
     sequential_result = pipeline.execute_sequentially(framework, ('initial input',))
