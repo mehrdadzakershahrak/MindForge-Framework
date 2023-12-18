6ad9becd70742f1042b8634e1dbbe123b57df15e
mehrdadzakershahrak <zaker.mehrdad@gmail.com>
2023-12-18 12:31:46 -0600

    updated language model to use gpt4-turbo

--- src/LanguageModelService.py
+++ src/LanguageModelService.py
@@ -10,7 +10,7 @@ class LanguageModelService:
         self.provider = provider
         self.openai_api_key = os.getenv("OPENAI_API_KEY")
 
-    def query_language_model(self, prompt, max_tokens=100):
+    def query_language_model(self, prompt, max_tokens=1000):
         if self.provider == 'openai':
             response = self.query_openai(prompt, max_tokens)
         elif self.provider == 'huggingface':
@@ -21,7 +21,8 @@ class LanguageModelService:
 
     def query_openai(self, prompt, max_tokens):
         openai.api_key = self.openai_api_key
-        response = openai.Completion.create(engine="davinci", prompt=prompt, max_tokens=max_tokens)
+        model = 'gpt-4-turbo'
+        response = openai.Completion.create(model, prompt=prompt, max_tokens=max_tokens)
         return response.choices[0].text.strip(), response.usage.total_tokens
 
     def query_huggingface(self, prompt, max_tokens):
