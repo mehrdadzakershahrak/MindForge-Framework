211843de41883b932fd5cf80018857ba36463cef
mehrdadzakershahrak <zaker.mehrdad@gmail.com>
2023-12-27 09:50:48 -0600

    MemStore added to apply Extremem ICL using enhanced Q-learning

---
+++ src/MemoryStore.py
@@ -0,0 +1,22 @@
+class MemoryStore:
+    def __init__(self):
+        self.data = []
+
+    def add_interaction(self, ai_response, human_feedback, similarity):
+        self.data.append((ai_response, human_feedback, similarity))
+
+    def apply_q_learning(self):
+        # Implement your Q-learning logic here
+        # Adjust AI behavior based on accumulated data
+        pass
+
+memory_store = MemoryStore()
+
+# After each interaction
+ai_response = "AI's response"
+human_feedback = input("Your feedback: ")
+similarity = compare_embeddings(ai_response, human_feedback)
+memory_store.add_interaction(ai_response, human_feedback, similarity)
+
+# Periodically or after certain conditions
+memory_store.apply_q_learning()
